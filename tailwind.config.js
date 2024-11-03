eb290aa2db0ee3802ec892f8b5ce199c5458b5a4
PooranaSelvan <pooranaselvan67@gmail.com>
2024-11-03 19:17:27 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        gee:'#009379'
+      },
+      fontFamily:{
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
